af385e458e594436ecf68781bafbef1d293443d9
chakri-seas <paladi.chakradhar01@gmail.com>
2023-12-12 18:32:02 -0500

    12-12

---
+++ Random/27_remove_element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i, idx=0, n = nums.size();
+        for(i=0;i<n;i++){
+            if(nums[i]!=val){
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+        return idx;
+    }
+};
